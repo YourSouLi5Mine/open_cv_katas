bdaee81ea6e849c1e7ef6ee6ee6415dfcf04df30
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-05-22 22:08:33 -0500

    fix max number in random

--- canvas.py
+++ canvas.py
@@ -5,12 +5,13 @@ import random
 canvas = np.zeros((500, 500, 3), dtype = "uint8")
 
 for _ in range(10):
-    x = random.randint(0, 500)
-    y = random.randint(0, 500)
+    x = random.randint(0, 499)
+    y = random.randint(0, 499)
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     canvas[x,y] = color
     radius = random.randint(3, 25)
     cv2.circle(canvas, (y, x), radius, color, 3)
 
 cv2.imshow("Canvas", canvas)
+cv2.imwrite('random_points.jpg', canvas)
 cv2.waitKey(0)
